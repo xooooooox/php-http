96eaa2e7e7677acf44cee34ea91c38c1fa370502
xooooooox <cosasce@gmail.com>
2021-10-19 10:11:38 +0800

    Update Curl.php

--- client/Curl.php
+++ client/Curl.php
@@ -11,7 +11,7 @@ class Curl
 
     /**
      * @param string $url
-     * @param array $header
+     * @param array $header ['Content-Type: application/json', 'Secret: 1234567890']
      * @return string
      */
     public static function Get(string $url = '', array $header = []) : string {
@@ -19,7 +19,7 @@ class Curl
         curl_setopt($curl, CURLOPT_URL, $url);
         curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
         curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
-        curl_setopt($curl, CURLOPT_HEADER, $header);
+        curl_setopt($curl, CURLOPT_HEADER, 1);
         curl_setopt($curl, CURLOPT_HTTPHEADER, $header);
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
         $response = curl_exec($curl);
@@ -33,7 +33,7 @@ class Curl
     /**
      * @param string $url
      * @param string $body
-     * @param array $header
+     * @param array $header ['Content-Type: application/json', 'Secret: 1234567890']
      * @return string
      */
     public static function Post(string $url= '', string $body = '', array $header = []) : string {
@@ -43,9 +43,38 @@ class Curl
         curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
         curl_setopt($curl, CURLOPT_POST, true);
         curl_setopt($curl, CURLOPT_POSTFIELDS, $body);
-        curl_setopt($curl, CURLOPT_HEADER, $header);
+        curl_setopt($curl, CURLOPT_HEADER, 1);
         curl_setopt($curl, CURLOPT_HTTPHEADER, $header);
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        $response = curl_exec($curl);
+        $error = curl_errno($curl);
+        curl_close($curl);
+        if ($error > 0) {
+            return '';
+        }
+        if (is_bool($response)){
+            return '';
+        }
+        return (string)$response;
+    }
+    
+    /**
+     * @param string $url
+     * @param string $body
+     * @param array $header ['Content-Type: application/json', 'Secret: 1234567890']
+     * @return string
+     */
+    public static function Delete(string $url= '', string $body = '', array $header = []) : string {
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+        curl_setopt($curl, CURLOPT_CUSTOMREQUEST, 'DELETE');
+        curl_setopt($curl, CURLOPT_POSTFIELDS, $body);
+        curl_setopt($curl, CURLOPT_HEADER, 1);
+        curl_setopt($curl, CURLOPT_HTTPHEADER, $header);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_TIMEOUT,5);
         $response = curl_exec($curl);
         $error = curl_errno($curl);
         curl_close($curl);
